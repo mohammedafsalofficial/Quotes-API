081e4df2e61bd102429259e7310f9ee29a403536
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-11-10 16:25:57 +0530

    Added Abdul Kalam quotes to the DB

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -6,29 +6,36 @@ const ProphetMuhammad = require("../models/prophetMuhammad.model.js");
 const AbdulKalam = require("../models/abdulKalam.model.js");
 const Buddha = require("../models/buddha.model.js");
 
-const prophetMuhammadCollection = ProphetMuhammad.collection;
+const abdulKalamCollection = AbdulKalam.collection;
 
-const prophetMuhammadQuotes = [
+const abdulKalamQuotes = [
   {
-    quote: "A good man treats women with honour.",
+    quote: "If you want to shine like a sun, first burn like a sun.",
   },
   {
-    quote: "Strive always to excel in virtue and truth.",
-  },
-  {
-    quote:
-      "The greatest jihad (struggle/striving) is to battle your own soul, to fight the evil within yourself.",
-  },
-  {
-    quote: "The strongest among you is the one who controls his anger.",
+    quote: "To succeed in your mission, you must have single-minded devotion to your goal.",
   },
   {
     quote:
-      "God does not look at your forms and possessions but he looks at your hearts and your deeds.",
+      "If four things are followed - having a great aim, acquiring knowledge, hard work, and perseverance - then anything can be achieved.",
+  },
+  {
+    quote:
+      "Don't take rest after your first victory because if you fail in second, more lips are waiting to say that your first victory was just luck.",
+  },
+  {
+    quote:
+      "Dream is not that which you see while sleeping it is something that does not let you sleep.",
+  },
+  {
+    quote: "A big shot is a little shot who keeps on shooting, so keep trying.",
+  },
+  {
+    quote: "Small aim is a crime; have great aim.",
   },
 ];
 
-prophetMuhammadCollection.insertMany(prophetMuhammadQuotes);
+abdulKalamCollection.insertMany(abdulKalamQuotes);
 
 const getQuotes = (req, res) => {
   res.json({
